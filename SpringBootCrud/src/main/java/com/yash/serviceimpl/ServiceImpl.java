21feec8d98f5de94b35880332ca9914455a3f9b2
shubham12jamkar <112398383+shubham12jamkar@users.noreply.github.com>
2022-09-20 17:40:08 +0530

    Add files via upload

--- SpringBootCrud/src/main/java/com/yash/serviceimpl/ServiceImpl.java
+++ SpringBootCrud/src/main/java/com/yash/serviceimpl/ServiceImpl.java
@@ -5,39 +5,50 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.yash.entity.patient;
+import com.yash.entity.Department;
+import com.yash.entity.Doctor;
+import com.yash.entity.Hospital;
+import com.yash.entity.Patient;
 import com.yash.repository.PatientRepo;
 import com.yash.service.PatientService;
 
 @Service
-public class ServiceImpl implements PatientService{
+public class ServiceImpl implements PatientService {
 
 	@Autowired
 	PatientRepo pr;
+
 	@Override
-	public patient addPatient(patient p) {
-		
+	public Patient addPatient(Patient p) {
+
 		return pr.save(p);
 	}
+
 	@Override
-	public List<patient> findAll() {
+	public List<Patient> findAll() {
 
 		return pr.findAll();
 	}
+
 	@Override
-	public patient findById(int pid) {
-		
+	public Patient findById(int pid) {
+
 		return pr.findById(pid).orElse(null);
 	}
+
 	@Override
-	public patient updatepat(patient p, int pid) {
+	public Patient updatepat(Patient p, int pid) {
 		p.setPid(pid);
-		
+
 		return pr.save(p);
 	}
-	
-	
-	
 
-	
+	@Override
+	public String deletePatient(int pid) {
+		Patient patient = findById(pid);
+		pr.deleteById(pid);
+		return "delete data successfully";
+
+	}
+
 }
