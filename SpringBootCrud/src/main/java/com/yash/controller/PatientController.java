b8d9a443e7fa39b575401b035b37791f4929f175
shubham12jamkar <112398383+shubham12jamkar@users.noreply.github.com>
2022-09-20 17:36:45 +0530

    Add files via upload

--- SpringBootCrud/src/main/java/com/yash/controller/PatientController.java
+++ SpringBootCrud/src/main/java/com/yash/controller/PatientController.java
@@ -3,6 +3,8 @@ package com.yash.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.yash.entity.patient;
+import com.yash.entity.Patient;
 import com.yash.service.PatientService;
 
 @RestController
@@ -21,34 +23,48 @@ public class PatientController {
 	@Autowired
 	PatientService ps;
 	
-	@RequestMapping(value = "/addPatient",method = RequestMethod.POST)
-	public patient addData(@RequestBody patient p)
+	@PostMapping("/addPatient")
+	public Patient addData(@RequestBody Patient p)
 	{
 		return ps.addPatient(p);
 		
 	}
 	
 	@PostMapping("/updatePatient/{pid}")
-	public patient updateData(@RequestBody patient p, @PathVariable int pid)
+	public Patient updateData(@RequestBody Patient p, @PathVariable int pid)
 	{
 		
 		return ps.updatepat(p,pid);
 		
 	}
 	
-	@RequestMapping(value = "/getPatient",method = RequestMethod.GET)
-	public List<patient> getData()
+	@GetMapping("/getPatient")
+	public List<Patient> getData()
+	{
+		return ps.findAll();
+		
+	}
+	@GetMapping(path="/getAllPatient", produces=MediaType.APPLICATION_XML_VALUE)
+	public List<Patient> getAllData()
 	{
 		return ps.findAll();
 		
 	}
 	
-	@RequestMapping(value = "/getSingleData/{pid}",method = RequestMethod.GET)
-	public patient getSingleData(@PathVariable int pid)
+	@GetMapping("/getSingleData/{pid}")
+	public Patient getSingleData(@PathVariable int pid)
 	{
 		return ps.findById(pid);
 		
-	}
+	} 
 
+	@DeleteMapping("/deletePatient/{pid}")
+	public String deleteData(@PathVariable  int pid)
+	{
+		ps.deletePatient(pid);
+		return "delete successfully";
+	
+		
+	}
 
 }
