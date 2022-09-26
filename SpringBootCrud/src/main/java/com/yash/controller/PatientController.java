87557599b668e07b538de04ea93c74472626c163
shubham12jamkar <112398383+shubham12jamkar@users.noreply.github.com>
2022-09-26 12:47:01 +0530

    Add files via upload

--- SpringBootCrud/src/main/java/com/yash/controller/PatientController.java
+++ SpringBootCrud/src/main/java/com/yash/controller/PatientController.java
@@ -22,49 +22,79 @@ public class PatientController {
 	
 	@Autowired
 	PatientService ps;
-	
+	//for json
 	@PostMapping("/addPatient")
 	public Patient addData(@RequestBody Patient p)
 	{
 		return ps.addPatient(p);
 		
 	}
+	//for xml
+	@PostMapping(path="/addPatient",produces = MediaType.APPLICATION_XML_VALUE)
+	public Patient addXMLData(@RequestBody Patient p)
+	{
+		System.out.println(p);
+		return ps.addPatient(p);
+		
+	}
 	
+	//for json
 	@PostMapping("/updatePatient/{pid}")
 	public Patient updateData(@RequestBody Patient p, @PathVariable int pid)
 	{
-		
-		return ps.updatepat(p,pid);
-		
+		return ps.updatepat(p,pid);	
+	}
+	//for xml
+	@PostMapping(path="/updatePatient/{pid}",produces = MediaType.APPLICATION_XML_VALUE)
+	public Patient updateXMLData(@RequestBody Patient p, @PathVariable int pid)
+	{
+		return ps.updatepat(p,pid);	
 	}
 	
+	//for json
 	@GetMapping("/getPatient")
-	public List<Patient> getData()
-	{
-		return ps.findAll();
-		
-	}
-	@GetMapping(path="/getAllPatient", produces=MediaType.APPLICATION_XML_VALUE)
 	public List<Patient> getAllData()
 	{
 		return ps.findAll();
 		
 	}
-	
+	//for xml
+	@GetMapping(path="/getAllPatient", produces=MediaType.APPLICATION_XML_VALUE)
+	public List<Patient> getAllXMLData()
+	{
+		return ps.findAll();
+		
+	}
+	//for json
 	@GetMapping("/getSingleData/{pid}")
 	public Patient getSingleData(@PathVariable int pid)
 	{
 		return ps.findById(pid);
 		
 	} 
-
+	//for xml
+	@GetMapping(path="/getSingleXMLData/{pid}",produces = MediaType.APPLICATION_XML_VALUE)
+	public Patient getSingleXMLData(@PathVariable int pid)
+	{
+		return ps.findById(pid);
+		
+	} 
+	//for json
 	@DeleteMapping("/deletePatient/{pid}")
 	public String deleteData(@PathVariable  int pid)
 	{
 		ps.deletePatient(pid);
 		return "delete successfully";
 	
-		
+	}
+	
+	//for xml
+	@DeleteMapping(path="/deletePatient/{pid}",produces = MediaType.APPLICATION_XML_VALUE)
+	public String deleteXMLData(@PathVariable  int pid)
+	{
+		ps.deletePatient(pid);
+		return "delete successfully";
+	
 	}
 
 }
